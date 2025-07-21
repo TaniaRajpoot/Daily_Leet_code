f8fb4bdb128335a29d1e64d166f71f3e0e8208dd
Tania Ashraf <151459794+TaniaRajpoot@users.noreply.github.com>
2025-07-21 09:49:29 +0500

    Time: 582 ms (6.99%) | Memory: 696.1 MB (5.01%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
+        string doublestring = s+s;
         int n = s.length();
 
-     
-        for(int l = 1; l <= n/2; l++) {
-            
-            if(n%l == 0) {
-                int times = n/l;
-                
-                string pattern   = s.substr(0, l);
-                string newStr = "";
-                while(times--) {
-                    newStr += pattern;
-                }
-                
-                if(newStr == s)
-                    return true;
+        for(int i = 1; i<n;i++){
+            string segment = doublestring.substr(i,n);
+            if(segment == s){
+                return true;
             }
-            
+           
         }
-        
-        return false;
+         return false;
     }
 };
