8be9d7f2cca5ce22e3dfbd7508688dc82edea0bd
Tania Ashraf <151459794+TaniaRajpoot@users.noreply.github.com>
2025-07-31 22:00:06 +0500

    Time: 0 ms (100.00%) | Memory: 30.2 MB (63.75%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int>sq;
-        for(int i =0; i<nums.size();i++){
-            sq.push_back(nums[i] * nums[i]);
-        }
-        sort(sq.begin(),sq.end());
-        return sq;
+       
 
+       for (int i = 0; i<nums.size(); i++){
+        nums[i] = nums[i]*nums[i];
+       }
+
+       sort(nums.begin(),nums.end());
+
+       return nums;
     }
 };
