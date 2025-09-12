cfd7baf0f919f8e40337602980a93708a2bf218f
Tania Ashraf <151459794+TaniaRajpoot@users.noreply.github.com>
2025-09-12 23:24:59 +0500

    Time: 9 ms (37.83%) | Memory: 140.8 MB (17.28%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
     vector<int> less, equal, greater;
 
-    for (int num : nums) {
+    for (auto num : nums) {
         if (num < pivot) less.push_back(num);
         else if (num == pivot) equal.push_back(num);
         else greater.push_back(num);
@@ -11,7 +11,7 @@ public:
 
     // Merge all together
     vector<int> result;
-    result.reserve(nums.size()); // reserve memory to avoid resizing
+    // result.reserve(nums.size()); // reserve memory to avoid resizing
     result.insert(result.end(), less.begin(), less.end());
     result.insert(result.end(), equal.begin(), equal.end());
     result.insert(result.end(), greater.begin(), greater.end());
